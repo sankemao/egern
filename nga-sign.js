39f45a100295ee2097531b640606de71a1228bf2
sankemao <210980059@qq.com>
2025-09-12 20:25:18 +0800

    temp

---
+++ nga-sign.js
@@ -0,0 +1,158 @@
+const NGA = $persistentStore.read("NGA");
+const cookie = NGA.cookie;
+const contentType = NGA.contentType;
+const userAgent = NGA.userAgent;
+const body = NGA.body;
+
+if (!cookie || !contentType || !userAgent || !body) {
+  $notification("NGA", "未获取cookie, 请先进入签到页面一次");
+  $done();
+} else {
+  !(async () => {
+    await checkin();
+    const mids = await missions();
+    for (const mid of mids) {
+      await checkInCountAdd(mid);
+    }
+  })()
+    .catch((e) => console.log(e))
+    .finally(() => $done());
+}
+
+function checkin() {
+  const newBody = { ...JSON.parse(body) };
+  newBody["__lib"] = "check_in";
+  newBody["__act"] = "check_in";
+  const promise = new Promise((resolve) => {
+    const options = {
+      url: "https://ngabbs.com/nuke.php",
+      headers: {
+        "Content-Type": contentType,
+        Cookie: cookie,
+        "User-Agent": userAgent,
+      },
+      body: ObjectToFormData(newBody, contentType),
+    };
+    $httpClient.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(err, resp);
+          $notification.post("NGA", "刮墙失败，详细参见日志", err);
+        } else if (resp.status === 200) {
+          console.log(data);
+          const result = JSON.parse(data);
+          if (result.error) {
+            $notification.post("NGA", "刮墙失败", result.error.join(";"));
+          } else if (result.data) {
+            const message = result.data[0];
+            const continued = result.data[1].continued;
+            const sum = result.data[1].sum;
+            $notification.post(
+              "NGA",
+              message,
+              `连续刮墙${continued}天，累计刮墙${sum}天`
+            );
+          }
+        }
+      } catch (e) {
+        console.log(e, resp);
+      } finally {
+        resolve();
+      }
+    });
+  });
+  return promise;
+}
+
+function missions() {
+  const promise = new Promise((resolve) => {
+    const newBody = { ...JSON.parse(body) };
+    newBody["__lib"] = "mission";
+    newBody["__act"] = "get_default";
+    newBody["get_success_repeat"] = "1";
+    newBody["no_compatible_fix"] = "1";
+    const options = {
+      url: "https://ngabbs.com/nuke.php",
+      headers: {
+        "Content-Type": contentType,
+        Cookie: cookie,
+        "User-Agent": userAgent,
+      },
+      body: ObjectToFormData(newBody, contentType),
+    };
+
+     $httpClient.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(err, resp);
+        } else {
+          const result = JSON.parse(data);
+          const mids = result.data[0].map((d) => d.id);
+          resolve(mids);
+        }
+      } catch (e) {
+        console.log(e, resp);
+        resolve([]);
+      }
+    });
+  });
+  return promise;
+}
+
+function checkInCountAdd(mid) {
+  const promise = new Promise((resolve) => {
+    const newBody = { ...JSON.parse(body) };
+    newBody["__lib"] = "mission";
+    newBody["__act"] = "checkin_count_add";
+    newBody["no_compatible_fix"] = "1";
+    newBody["mid"] = mid;
+    const options = {
+      url: "https://ngabbs.com/nuke.php",
+      headers: {
+        "Content-Type": contentType,
+        Cookie: cookie,
+        "User-Agent": userAgent,
+      },
+      body: ObjectToFormData(newBody, contentType),
+    };
+
+    $httpClient.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(err, resp);
+        } else {
+          console.log(`mission:${mid}`);
+        }
+      } catch (e) {
+        console.log(e, resp);
+      } finally {
+        resolve();
+      }
+    });
+  });
+  return promise;
+}
+
+function ObjectToFormData(object, contentType) {
+  const boundary = contentType.split("; ")[1].split("=")[1];
+  const splitBoundary = `--${boundary}`;
+  var body = `${splitBoundary}\r\n`;
+  const array = [];
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      array.push({
+        name: key,
+        value: object[key],
+      });
+    }
+  }
+  var data = array.map((element) => {
+    var name = `Content-Disposition: form-data; name="${element.name}"`;
+    var entityString = `${name}\r\n\r\n${element.value}`;
+    return entityString;
+  });
+  body = `${body}${data.join(
+    `\r\n${splitBoundary}\r\n`
+  )}\r\n${splitBoundary}--\r\n`;
+  return body;
+}
--- nga.js
+++ nga.js
@@ -1,2 +1,62 @@
-$notification.post("test");
-$done();
+!(async () => {
+  if ($request.body && $request.body.includes("------WebKitForm")) {
+    const cookie = $request.headers["cookie"];
+    const contentType = $request.headers["content-type"];
+    const userAgent = $request.headers["user-agent"];
+    const body = $request.body;
+    var obj = FormDataToObject(body, contentType);
+    if (obj["__lib"] === "mission" && obj["__act"] === "get_default") {
+      var obj = FormDataToObject(body, contentType);
+      $persistentStore.write(
+        JSON.stringify({
+          userAgent,
+          contentType,
+          cookie,
+          body: obj,
+        }),
+        "NGA"
+      );
+      $notification.post("获取NGA cookie", "成功");
+    }
+  }
+})()
+  .catch((e) => console.log("解析cookie错误" + e.toString()))
+  .finally(() => $done({}));
+
+function FormDataToObject(form, contentType) {
+  const boundary = contentType.split("; ")[1].split("=")[1];
+  const splitBoundary = `--${boundary}`;
+  const index = form.indexOf(splitBoundary);
+  form = form.substr(index);
+  const lastIndex = form.lastIndexOf(splitBoundary);
+  form = form.substring(0, lastIndex);
+  const array = compact(form.split(splitBoundary)).map((a) => {
+    const entity = compact(a.split("\r\n"));
+    const regex = /Content-Disposition: form-data; name="(.*)"/;
+    var matchs = regex.exec(entity[0]);
+    return {
+      name: matchs[1],
+      value: entity[1],
+    };
+  });
+
+  function compact(array) {
+    let resIndex = 0;
+    const result = [];
+    if (array == null) {
+      return result;
+    }
+    for (const value of array) {
+      if (value) {
+        result[resIndex++] = value;
+      }
+    }
+    return result;
+  }
+
+  const result = {};
+  array.forEach((a) => {
+    result[a.name] = a.value;
+  });
+  return result;
+}
